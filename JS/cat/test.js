4fb5d7363769390be57ee37c4bb9fffe7a3fabf8
vsorg <zhangjie1314@gmail.com>
2025-07-24 23:01:22 +0800

    1

--- JS/cat/test.js
+++
@@ -1,9 +0,0 @@
-import {} from './wrapper/index.js';
-
-// import { test } from './testVideo.js';
-import { test } from './testAlist.js';
-
-(async function () {
-    await test();
-    debugger;
-})();
